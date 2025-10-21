c975caa9d5731036e775aa0526b4b0ffac659fc3
itzpraveen <itzpraveen@users.noreply.github.com>
2025-10-21 14:10:01 +0530

    Serve media uploads in production

--- waterlab/urls.py
+++ waterlab/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include  # Added include
 from django.views.generic import RedirectView
@@ -25,3 +27,6 @@ urlpatterns = [
     path('sw.js', RedirectView.as_view(url='/static/sw.js', permanent=True)),
     path('', include('core.urls', namespace='core')),
 ]
+
+if settings.MEDIA_URL:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
